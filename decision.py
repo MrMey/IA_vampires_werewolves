285492d0f67be0bca7dff19117f5d94f4368d16b
Alexandre Sioufi <sioufia78@gmail.com>
2018-03-02 12:31:30 +0100

    Added the basic split at the beginning

--- decision.py
+++ decision.py
@@ -5,6 +5,7 @@ Created on Fri Feb  2 11:29:05 2018
 @author: Mr_Mey
 """
 import struct
+from math import ceil
 
 from algorithm.greedy import get_closest_point, get_distance,get_dest
 
@@ -15,22 +16,33 @@ class Actor:
         self.target = []
 
     def action(self,grid):
-        for ally in grid.allies:
-            print("humans: {}".format(grid.humans))
-            print("allies: {}".format(grid.allies))
-            print("enemies: {}".format(grid.enemies))
-            if self.algorithm == 1:
-                dest = get_dest(grid, ally)
+        if self.algorithm == 1:
+            if len(grid.allies) == 1:
+                print("Séparation")
+                dest = get_dest(grid, list(grid.allies.keys())[0])
+                move = [list(grid.allies.keys())[0][0],list(grid.allies.keys())[0][1],ceil(list(grid.allies.values())[0]/2),dest[0],dest[1]]
+                self.queue.append(move)
+                grid.add_locked_cell(dest)
+            else: 
+                print("No separation")
+                for ally in grid.allies:
+                    print("humans: {}".format(grid.humans))
+                    print("allies: {}".format(grid.allies))
+                    print("enemies: {}".format(grid.enemies))
+                    if self.algorithm == 1:
+                        dest = get_dest(grid, ally)
 
-            elif self.algorithm == 2:
-                #dest = ...
-                pass #to link with alpha beta algo
-            print("dest : {}".format(dest))
-            move = [ally[0],ally[1],grid.get_group_at(ally[0],ally[1]),dest[0],dest[1]]
-            print("move : {}".format(move))
+        
+                    print("dest : {}".format(dest))
+                    move = [ally[0],ally[1],grid.get_group_at(ally[0],ally[1]),dest[0],dest[1]]
+                    print("move : {}".format(move))
 
-            self.queue.append(move)
-            grid.add_locked_cell(dest)
+                    self.queue.append(move)
+                    grid.add_locked_cell(dest)
+        elif self.algorithm == 2:
+            #dest = ...
+            pass #to link with alpha beta algo
+        
 
     def send_moves(self):
         paquet = bytes()
